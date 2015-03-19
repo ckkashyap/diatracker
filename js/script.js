473f6947bbd4b99f32f2b8de84f8289efbf027ec
CK Kashyap <ckkashyap@gmail.com>
2015-03-19 18:42:06 +0530

    text field added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,2 @@
 
-var x = 1;
 
-function setClass(idx, className) {
-    var id = 'page' + idx;
-    var div = document.getElementById(id);
-    div.className = className;
-}
-
-function switchPage() {
-    var px = x;
-    x = x + 1;
-    if ( x > 2) {
-	x = 1;
-    }
-    setClass(x, "fullpageVisible");
-    setClass(px, "fullpageInvisible");
-}
